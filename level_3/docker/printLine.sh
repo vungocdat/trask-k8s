29f2e529c369f9cb9137c9e188d5cc4cbc9d7fb2
Vu Ngoc Dat <ngoc.dat.vu@seznam.cz>
2021-08-11 14:08:18 +0200

    working homework level 3

--- level_3/docker/printLine.sh
+++ level_3/docker/printLine.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 while :
-do { echo -e 'HTTP/1.1 200 OK\r\n'; shuf -n 1 level3file.txt; } | nc -l 8080; 
+do { echo -e 'HTTP/1.1 200 OK\r\n'; shuf -n 1 level3file.txt; } | nc -l 8080 
 done
